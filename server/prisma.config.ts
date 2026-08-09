f7e0b1295cc2c7a47330b0ea034b2285e9464e41
GG <gg@gb-messenger.app>
2026-08-09 19:59:18 +0300

    Fix prisma config: fallback to POSTGRES_PASSWORD

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || `postgresql://gbuser:${process.env.POSTGRES_PASSWORD}@postgres:5432/gb_messenger?schema=public`,
   },
 });
