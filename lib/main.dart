9921f80c5294c7e5951dcaa4341e13dfcaa9a94e
GG <gg@gb-messenger.app>
2026-08-09 16:00:38 +0300

    Add E2E encryption for text messages
    - Added EncryptionService for server (AES-256-GCM)
    - Added EncryptionService for mobile app
    - Messages encrypted before saving to DB
    - Messages decrypted when reading
    - Added ENCRYPTION_KEY config support

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'core/app_state.dart';
+import 'core/encryption_service.dart';
 import 'core/models.dart';
 import 'core/push_service.dart';
 import 'core/update_service.dart';
@@ -21,8 +22,9 @@ import 'theme/app_theme.dart';
 import 'theme/notification_banner.dart';
 import 'theme/theme_scope.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EncryptionService.instance.init();
   runApp(
     ChangeNotifierProvider(
       create: (_) => AppState()..init(),
