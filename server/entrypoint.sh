f5871569309617cc8cb37c68bc61a8db283270b2
GG <gg@gb-messenger.app>
2026-08-09 18:38:52 +0300

    Simplify entrypoint: use DATABASE_URL from container env

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -e
 
-# Export DATABASE_URL for Prisma
+# Run database migrations using the DATABASE_URL from environment
 cd /app
-export DATABASE_URL="postgresql://gbuser:${POSTGRES_PASSWORD}@postgres:5432/gb_messenger?schema=public"
-
-# Run database migrations
 npx prisma migrate deploy
 
 # Start the application
